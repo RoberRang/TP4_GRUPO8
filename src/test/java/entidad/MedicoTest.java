343a560b7952af68feed3c3d5758ef6414334887
Roberto Rangogni <rob_rang@hotmail.com>
2024-05-16 20:14:08 -0300

    Test Medico

---
+++ src/test/java/entidad/MedicoTest.java
@@ -0,0 +1,130 @@
+package entidad;
+
+import org.junit.Test;
+
+public class MedicoTest {
+    @Test
+    public void testGetApellido() {
+
+    }
+
+    @Test
+    public void testGetCorreo() {
+
+    }
+
+    @Test
+    public void testGetDirección() {
+
+    }
+
+    @Test
+    public void testGetEspecialidad() {
+
+    }
+
+    @Test
+    public void testGetFNac() {
+
+    }
+
+    @Test
+    public void testGetLegajo() {
+
+    }
+
+    @Test
+    public void testGetListaTurnos() {
+
+    }
+
+    @Test
+    public void testGetLocalidad() {
+
+    }
+
+    @Test
+    public void testGetNombre() {
+
+    }
+
+    @Test
+    public void testGetSexo() {
+
+    }
+
+    @Test
+    public void testGetTeléfono() {
+
+    }
+
+    @Test
+    public void testGetUsuario() {
+
+    }
+
+    @Test
+    public void testSetApellido() {
+
+    }
+
+    @Test
+    public void testSetCorreo() {
+
+    }
+
+    @Test
+    public void testSetDirección() {
+
+    }
+
+    @Test
+    public void testSetEspecialidad() {
+
+    }
+
+    @Test
+    public void testSetFNac() {
+
+    }
+
+    @Test
+    public void testSetLegajo() {
+
+    }
+
+    @Test
+    public void testSetListaTurnos() {
+
+    }
+
+    @Test
+    public void testSetLocalidad() {
+
+    }
+
+    @Test
+    public void testSetNombre() {
+
+    }
+
+    @Test
+    public void testSetSexo() {
+
+    }
+
+    @Test
+    public void testSetTeléfono() {
+
+    }
+
+    @Test
+    public void testSetUsuario() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+}
