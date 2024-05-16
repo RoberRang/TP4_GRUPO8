9ec0d8f4f9fa429f4eb7199df8d4247f5ce1f225
Roberto Rangogni <rob_rang@hotmail.com>
2024-05-16 20:00:05 -0300

    Prueba de medico

--- src/main/java/UTNFRGP/TP4_GRUPO8/App.java
+++ src/main/java/UTNFRGP/TP4_GRUPO8/App.java
@@ -1,5 +1,9 @@
 package UTNFRGP.TP4_GRUPO8;
 
+import java.time.LocalDate;
+
+import entidad.Medico;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Medico med1 = new Medico();
+		med1.setNombre("Daniel");
+		med1.setApellido("Diaz");
+		med1.setSexo('M');
+		med1.setFNac(LocalDate.parse("1988-05-24"));
+		med1.setDirección("Calle Nula 123");
+		med1.setLocalidad("Belgrano");
+		med1.setCorreo("danieldiaz@intramed.net");
+		med1.setTeléfono("1167891452");
     }
 }
