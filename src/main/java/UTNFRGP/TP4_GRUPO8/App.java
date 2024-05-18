7132eed6b700a0f653e558e3649d06199aa8a26e
Gabriel Vargas <ecgabrielec@hotmail.com>
2024-05-17 21:24:13 -0300

    Carga de medicos BD

--- src/main/java/UTNFRGP/TP4_GRUPO8/App.java
+++ src/main/java/UTNFRGP/TP4_GRUPO8/App.java
@@ -2,6 +2,7 @@ package UTNFRGP.TP4_GRUPO8;
 
 import java.time.LocalDate;
 
+import dao.daoMedico;
 import entidad.Medico;
 
 /**
@@ -12,14 +13,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        Medico med1 = new Medico();
-		med1.setNombre("Daniel");
-		med1.setApellido("Diaz");
-		med1.setSexo('M');
-		med1.setFNac(LocalDate.parse("1988-05-24"));
-		med1.setDirección("Calle Nula 123");
-		med1.setLocalidad("Belgrano");
-		med1.setCorreo("danieldiaz@intramed.net");
-		med1.setTeléfono("1167891452");
+    	
+    	daoMedico daomed = new daoMedico();
+
+        for (int i= 1; i<13; i++) {
+    		Medico med1 = new Medico();
+            med1.setNombre("Medico_Nom_"+i);
+            med1.setApellido("Medico_Ape_"+i);
+            med1.setSexo('M');
+            med1.setFNac(LocalDate.parse("1988-05-01"));
+            med1.setDirección("Calle Nula "+i);
+            med1.setLocalidad("Localidad_"+i);
+            med1.setCorreo("medico_"+i+"_@intramed.net");
+            med1.setTeléfono("telefono_"+i);
+            daomed.Add(med1);
+    	}
+				
     }
 }
