ac6720e279360c32407053b08a164c91bc6106c0
Gabriel Vargas <ecgabrielec@hotmail.com>
2024-05-17 21:55:59 -0300

    Carga de relacion Especialida/Medico

--- src/main/java/UTNFRGP/TP4_GRUPO8/App.java
+++ src/main/java/UTNFRGP/TP4_GRUPO8/App.java
@@ -1,8 +1,11 @@
 package UTNFRGP.TP4_GRUPO8;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import dao.daoEspecialidad;
 import dao.daoMedico;
+import entidad.Especialidad;
 import entidad.Medico;
 
 /**
@@ -13,21 +16,40 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
-    	daoMedico daomed = new daoMedico();
+    	daoEspecialidad daoesp = new daoEspecialidad();
 
-        for (int i= 1; i<13; i++) {
-    		Medico med1 = new Medico();
-            med1.setNombre("Medico_Nom_"+i);
-            med1.setApellido("Medico_Ape_"+i);
-            med1.setSexo('M');
-            med1.setFNac(LocalDate.parse("1988-05-01"));
-            med1.setDirección("Calle Nula "+i);
-            med1.setLocalidad("Localidad_"+i);
-            med1.setCorreo("medico_"+i+"_@intramed.net");
-            med1.setTeléfono("telefono_"+i);
-            daomed.Add(med1);
+    	
+    	for (int i = 1; i < 13; i++) {
+    	    Especialidad espe = new Especialidad();
+    	    espe.setNombre("Gastroenterologia_" + i);
+    	    daoesp.Add(espe);
     	}
+
+    	daoMedico daomed = new daoMedico();
+    	List<Especialidad> especialidades = daoesp.ReadAll();
+
+    	for (int i = 1; i < 13; i++) {
+    	    Medico med1 = new Medico();
+    	    med1.setNombre("Medico_Nom_" + i);
+    	    med1.setApellido("Medico_Ape_" + i);
+    	    med1.setSexo('M');
+    	    med1.setFNac(LocalDate.parse("1988-05-01"));
+    	    med1.setDirección("Calle Nula " + i);
+    	    med1.setLocalidad("Localidad_" + i);
+    	    med1.setCorreo("medico_" + i + "_@intramed.net");
+    	    med1.setTeléfono("telefono_" + i);
+    	    
+    	    // Asignacion de una especialidad al médico 
+    	    // asignamos una especialidad a cada médico de forma secuencial
+    	    Especialidad especialidad = especialidades.get(i - 1); // con i-1 porque el indice arranca en 0
+    	    med1.setEspecialidad(especialidad);
+    	    
+    	    daomed.Add(med1);
+    	}
+
+    
+        
+        
 				
     }
 }
