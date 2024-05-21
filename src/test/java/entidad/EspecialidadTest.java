0212d44aa8bb2c43ebef29f84a22de10f88fb63e
Roberto Rangogni <rob_rang@hotmail.com>
2024-05-21 13:16:50 -0300

    Create EspecialidadTest.java

---
+++ src/test/java/entidad/EspecialidadTest.java
@@ -0,0 +1,30 @@
+package entidad;
+
+import org.junit.Test;
+
+public class EspecialidadTest {
+    @Test
+    public void testGetId() {
+
+    }
+
+    @Test
+    public void testGetNombre() {
+
+    }
+
+    @Test
+    public void testSetId() {
+
+    }
+
+    @Test
+    public void testSetNombre() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+}
