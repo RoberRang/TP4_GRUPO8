0f9c941699242afeda8d5248a94b338634bff5cc
Roberto Rangogni <rob_rang@hotmail.com>
2024-05-23 04:24:53 -0300

    Punto 5 Otra Forma

--- src/main/java/UTNFRGP/TP4_GRUPO8/App2.java
+++ src/main/java/UTNFRGP/TP4_GRUPO8/App2.java
@@ -15,129 +15,139 @@ public class App2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    
-		 // 1- Mostrar los médicos ordenados por legajo de mayor a menor. Los campos que se deben mostrar son todos. HQL SELECT ALL.
-	      
+
+		// 1- Mostrar los médicos ordenados por legajo de mayor a menor. Los campos que
+		// se deben mostrar son todos. HQL SELECT ALL.
+
 		daoMedico medicoDao = new daoMedico();
 		
-	
-         List<Medico> medicos = medicoDao.mostrarMedicosOrdenadosPorLegajoDesc();
-	    
-	    
-	    for (Medico medico : medicos) {
-	        int legajo = (int) medico.getLegajo(); 
-	        String nombre = (String) medico.getNombre(); 
-	        String apellido = (String) medico.getApellido(); 
-	        
-	        System.out.println("Legajo: " + legajo + ", Nombre: " + nombre + ", Apellido: " + apellido);
-	    }
-		
-		//2) Mostrar todos los médicos ordenados según su legajo de menor a mayor, mostrar son legajo, nombre y apellido. Realizarlo SELECT SOME COLUMNS
-	
-	    
-	    List<Object[]> medicosAsc = medicoDao.mostrarMedicosOrdenadosPorLegajoAsc();
-	     
-	    
-	    
-	    for (Object[] medico : medicosAsc) {
-            int legajo = (int) medico[0];
-            String nombre = (String) medico[1];
-            String apellido = (String) medico[2];
-	        
-	        System.out.println("Legajo: " + legajo + ", Nombre: " + nombre + ", Apellido: " + apellido);
-	    }
-		
-		//3) Mostrar todos los turnos que posee el médico con legajo 1234 en el día 01/01/2025 
-		//Los campos que se deben mostrar son legajo, fecha de alta y estado. Realizarlo como se explica en el video HQL INNER JOIN 
-		
-		daoTurno turnoDao = new daoTurno();
-        
-        /*int legajoMedico = 1234;
-        LocalDate fecha = LocalDate.of(2025, 1, 1);
-        
-        
-        Turno turno = turnoDao.mostrarTurnoMedicoPorLegajoYFecha(legajoMedico, fecha);
-        
-   
-        if (turno != null) {
-            System.out.println(turno.toString());
-        } else {
-            System.out.println("No se encontró ningún turno para el médico con legajo " + legajoMedico + " en la fecha " + fecha);
-        }*/
-		
-		int legajoMedico = 3;
-        String fecha = "2025-01-01";
-        
-        List<Object[]> listaTurnos = (List<Object[]>)medicoDao.MostrarListadoTurnosPorLegajoYFecha(legajoMedico, fecha);
-        
-        if (listaTurnos.isEmpty()) {
-        	System.out.println("No se encontró ningún turno para el médico con legajo " + legajoMedico + " en la fecha " + fecha);
-        } else {
-        
-        	for (Object[] objeto : listaTurnos) {
-        		Integer legajo = (Integer)objeto[0];
-        		String date = objeto[1].toString();
-        		String estado = objeto[2].toString();
-        		System.out.println("Legajo: " + legajo + ", Fecha: " + date + ", Estado: " + estado + ".");
-        	}
-        }
-        
-		
-		//4) Mostrar todos los legajos de los médicos  El campo que se debe mostrar es legajo. 
-		//daoMedico medicoDao = new daoMedico();
-        
-        // Obtener y mostrar todos los legajos de los médicos
-        List<Integer> legajos = medicoDao.obtenerLegajosMedicos();
-        
-        // Mostrar los legajos
-        for (Integer legajo : legajos) {
-            System.out.println(legajo);
-        }
-        
-        //5 5) Mostrar el médico con mayor número de legajo 
-        //daoMedico medicoDao = new daoMedico();
-        
-       
-        int maxLegajo = medicoDao.obtenerMaxLegajoMedico();
-        System.out.println("Máximo número de legajo entre los médicos: " + maxLegajo);
-		
-		//daoTurno turnoDao = new daoTurno();
-	        
-	        String fechaInicio = "1988-05-01";
-	        String fechaFin = "2025-01-01";
-	        
-	        EstadoTurno ausente = EstadoTurno.AUSENTE;
-	        EstadoTurno presente = EstadoTurno.PRESENTE;
-	       /* EstadoTurno pendiente = EstadoTurno.PENDIENTE;*/
-	        
-	        double porcentajeAusentes = turnoDao.obtenerPorcentajeTurnos(ausente, fechaInicio, fechaFin);
-	        double porcentajePresentes = turnoDao.obtenerPorcentajeTurnos(presente, fechaInicio, fechaFin);
-	       /* double porcentajePendientes = turnoDao.obtenerPorcentajeTurnos(pendiente, fechaInicio, fechaFin);*/
-	        
-	        System.out.println("Porcentaje de turnos ausentes: " + porcentajeAusentes + "%");
-	        System.out.println("Porcentaje de turnos presentes: " + porcentajePresentes + "%");
-	       /* System.out.println("Porcentaje de turnos pendientes: " + porcentajePendientes + "%");*/
-	}
 
-	//Otra forma punto 4 
-	
-	/*	4) Mostrar todos los legajos de los médicos 
-		El campo que se debe mostrar es legajo. Realizarlo como se explica en el video HQL 
-		SELECT ONE COLUMN
+		List<Medico> medicos = medicoDao.mostrarMedicosOrdenadosPorLegajoDesc();
 
-	 * */
-		public static void ReadPunto4() {
-		ConfigHibernate hb = new ConfigHibernate();
-		Session session= hb.abrirConexion();
-		
-		List<Integer>listaLegajoMedicos = (List<Integer>)session.createQuery("SELECT md.legajo FROM Medico md").list();
-			
-		for (Integer legajo: listaLegajoMedicos) {
+		for (Medico medico : medicos) {
+			int legajo = (int) medico.getLegajo();
+			String nombre = (String) medico.getNombre();
+			String apellido = (String) medico.getApellido();
 
-			System.out.println("Legajo medico: "+legajo);
+			System.out.println("Legajo: " + legajo + ", Nombre: " + nombre + ", Apellido: " + apellido);
 		}
+
+		// 2) Mostrar todos los médicos ordenados según su legajo de menor a mayor,
+		// mostrar son legajo, nombre y apellido. Realizarlo SELECT SOME COLUMNS
+
+		List<Object[]> medicosAsc = medicoDao.mostrarMedicosOrdenadosPorLegajoAsc();
+
+		for (Object[] medico : medicosAsc) {
+			int legajo = (int) medico[0];
+			String nombre = (String) medico[1];
+			String apellido = (String) medico[2];
+
+			System.out.println("Legajo: " + legajo + ", Nombre: " + nombre + ", Apellido: " + apellido);
+		}
+
+		// 3) Mostrar todos los turnos que posee el médico con legajo 1234 en el día
+		// 01/01/2025
+		// Los campos que se deben mostrar son legajo, fecha de alta y estado.
+		// Realizarlo como se explica en el video HQL INNER JOIN
+
 		
-		hb.cerrarConexion();
+
+		/*
+		 * int legajoMedico = 1234;
+		 * LocalDate fecha = LocalDate.of(2025, 1, 1);
+		 * 
+		 * 
+		 * Turno turno = turnoDao.mostrarTurnoMedicoPorLegajoYFecha(legajoMedico,
+		 * fecha);
+		 * 
+		 * 
+		 * if (turno != null) {
+		 * System.out.println(turno.toString());
+		 * } else {
+		 * System.out.println("No se encontró ningún turno para el médico con legajo " +
+		 * legajoMedico + " en la fecha " + fecha);
+		 * }
+		 */
+
+		int legajoMedico = 3;
+		String fecha = "2025-01-01";
+
+		List<Object[]> listaTurnos = (List<Object[]>) medicoDao.MostrarListadoTurnosPorLegajoYFecha(legajoMedico,
+				fecha);
+
+		if (listaTurnos.isEmpty()) {
+			System.out.println(
+					"No se encontró ningún turno para el médico con legajo " + legajoMedico + " en la fecha " + fecha);
+		} else {
+
+			for (Object[] objeto : listaTurnos) {
+				Integer legajo = (Integer) objeto[0];
+				String date = objeto[1].toString();
+				String estado = objeto[2].toString();
+				System.out.println("Legajo: " + legajo + ", Fecha: " + date + ", Estado: " + estado + ".");
+			}
+		}
+
+		// 4) Mostrar todos los legajos de los médicos El campo que se debe mostrar es
+		// legajo.
+		// daoMedico medicoDao = new daoMedico();
+
+		// Obtener y mostrar todos los legajos de los médicos
+		List<Integer> legajos = medicoDao.obtenerLegajosMedicos();
+
+		// Mostrar los legajos
+		for (Integer legajo : legajos) {
+			System.out.println(legajo);
+		}
+
+		// 5 5) Mostrar el médico con mayor número de legajo
+		// daoMedico medicoDao = new daoMedico();
+
+		int maxLegajo = medicoDao.obtenerMaxLegajoMedico();
+		System.out.println("Máximo número de legajo entre los médicos: " + maxLegajo);
+
+		daoTurno turnoDao = new daoTurno();		
+
+		String fechaInicio = "1988-05-01";
+		String fechaFin = "2025-01-01";
+
+		EstadoTurno ausente = EstadoTurno.AUSENTE;
+		EstadoTurno presente = EstadoTurno.PRESENTE;		
+		EstadoTurno pendiente = EstadoTurno.PENDIENTE;
+
+		double porcentajeAusentes = turnoDao.obtenerPorcentajeTurnos(ausente, fechaInicio, fechaFin);
+		double porcentajePresentes = turnoDao.obtenerPorcentajeTurnos(presente, fechaInicio, fechaFin);		
+		double porcentajePendientes = turnoDao.getPorcentajeTotalTurnosPorEstado(pendiente, fechaInicio, fechaFin);		 
+
+		System.out.println("Porcentaje de turnos ausentes: " + porcentajeAusentes + "%");
+		System.out.println("Porcentaje de turnos presentes: " + porcentajePresentes + "%");
+		System.out.println("Porcentaje de turnos Pendidntes:" + porcentajePendientes + "%");
+
 	}
+
+	// Otra forma punto 4
+
+	/*
+	 * 4) Mostrar todos los legajos de los médicos
+	 * El campo que se debe mostrar es legajo. Realizarlo como se explica en el
+	 * video HQL
+	 * SELECT ONE COLUMN
+	 * 
+	 * 
+	 * public static void ReadPunto4() {
+	 * ConfigHibernate hb = new ConfigHibernate();
+	 * Session session= hb.abrirConexion();
+	 * 
+	 * List<Integer>listaLegajoMedicos =
+	 * (List<Integer>)session.createQuery("SELECT md.legajo FROM Medico md").list();
+	 * 
+	 * for (Integer legajo: listaLegajoMedicos) {
+	 * 
+	 * System.out.println("Legajo medico: "+legajo);
+	 * }
+	 * 
+	 * hb.cerrarConexion();
+	 * }
+	 */
 
 }
--- src/main/java/dao/daoTurno.java
+++ src/main/java/dao/daoTurno.java
@@ -3,7 +3,6 @@ package dao;
 import java.util.List;
 import java.time.LocalDate;
 
-
 import org.hibernate.Session;
 
 import entidad.EstadoTurno;
@@ -12,123 +11,157 @@ import entidad.Turno;
 public class daoTurno {
 
 	public void Add(Turno turno) {
-		
+
 		ConfigHibernate ch = new ConfigHibernate();
 		Session session = ch.abrirConexion();
-		
+
 		session.beginTransaction();
 		session.save(turno);
-		
+
 		session.getTransaction().commit();
 		ch.cerrarConexion();
-		
+
 	}
-	
 
 	public List<Turno> ReadAll() {
-		
+
 		ConfigHibernate ch = new ConfigHibernate();
 		Session session = ch.abrirConexion();
-		
+
 		session.beginTransaction();
-		List<Turno> list = (List<Turno>)session.createQuery("from Turno").list();
-		
+		List<Turno> list = (List<Turno>) session.createQuery("from Turno").list();
+
 		ch.cerrarConexion();
-		
+
 		return list;
 	}
-	
 
 	public void Update(Turno turno) {
-		
+
 		ConfigHibernate ch = new ConfigHibernate();
 		Session session = ch.abrirConexion();
-		
+
 		session.beginTransaction();
 		session.update(turno);
 		session.getTransaction().commit();
-		
+
 		ch.cerrarConexion();
 	}
-	
 
 	public void Delete(Turno turno) {
-		
+
 		ConfigHibernate ch = new ConfigHibernate();
 		Session session = ch.abrirConexion();
-		
+
 		session.beginTransaction();
 		session.delete(turno);
 		session.getTransaction().commit();
-		
+
 		ch.cerrarConexion();
 	}
-	
-	/*public Turno mostrarTurnoMedicoPorLegajoYFecha(int legajoMedico, LocalDate fecha) {
-        ConfigHibernate ch = new ConfigHibernate();
-        Session session = ch.abrirConexion();
-        
-        session.beginTransaction();
-        
-        // Consulta HQL para obtener el turno del médico con el legajo especificado en la fecha especificada
-        Turno turno = (Turno) session.createQuery(
-                "SELECT t FROM Turno t " +
-                "WHERE t.medico.legajo = :legajoMedico " +
-                "AND t.fecha = :fecha")
-                .setParameter("legajoMedico", legajoMedico)
-                .setParameter("fecha", fecha.toString()) // Convierte LocalDate a String
-                .uniqueResult();
-        
-        session.getTransaction().commit();
-        ch.cerrarConexion();
-        
-        return turno;
-    }*/
 
-	//punto6
+	/*
+	 * public Turno mostrarTurnoMedicoPorLegajoYFecha(int legajoMedico, LocalDate
+	 * fecha) {
+	 * ConfigHibernate ch = new ConfigHibernate();
+	 * Session session = ch.abrirConexion();
+	 * 
+	 * session.beginTransaction();
+	 * 
+	 * // Consulta HQL para obtener el turno del médico con el legajo especificado
+	 * en la fecha especificada
+	 * Turno turno = (Turno) session.createQuery(
+	 * "SELECT t FROM Turno t " +
+	 * "WHERE t.medico.legajo = :legajoMedico " +
+	 * "AND t.fecha = :fecha")
+	 * .setParameter("legajoMedico", legajoMedico)
+	 * .setParameter("fecha", fecha.toString()) // Convierte LocalDate a String
+	 * .uniqueResult();
+	 * 
+	 * session.getTransaction().commit();
+	 * ch.cerrarConexion();
+	 * 
+	 * return turno;
+	 * }
+	 */
+
+	// punto6
 	public double obtenerPorcentajeTurnos(EstadoTurno estado, String fechaInicio, String fechaFin) {
-        ConfigHibernate ch = new ConfigHibernate();
-        Session session = ch.abrirConexion();
-        
-        session.beginTransaction();
-        
-        // Consulta HQL para obtener la cantidad de turnos en el estado especificado entre las fechas especificadas
-        Long cantidadTurnos = (Long) session.createQuery(
-                "SELECT COUNT(t) FROM Turno t " +
-                "WHERE t.estado = :estado " +
-                "AND t.fecha BETWEEN :fechaInicio AND :fechaFin")
-                .setParameter("estado", estado)
-                .setParameter("fechaInicio", fechaInicio)
-                .setParameter("fechaFin", fechaFin)
-                .uniqueResult();
-        
-        session.getTransaction().commit();
-        ch.cerrarConexion();
-        
-        // Obtener el total de turnos entre las fechas dadas
-        long totalTurnos = obtenerTotalTurnos(fechaInicio, fechaFin);
-        
-        // Calcular el porcentaje de turnos en el estado especificado
-        return (double) cantidadTurnos / totalTurnos * 100;
-    }
-	private long obtenerTotalTurnos(String fechaInicio, String fechaFin) {
-	    ConfigHibernate ch = new ConfigHibernate();
-	    Session session = ch.abrirConexion();
-	    
-	    session.beginTransaction();
-	    
-	    // Consulta HQL para obtener el total de turnos entre las fechas especificadas
-	    Long totalTurnos = (Long) session.createQuery(
-	            "SELECT COUNT(t) FROM Turno t " +
-	            "WHERE t.fecha BETWEEN :fechaInicio AND :fechaFin")
-	            .setParameter("fechaInicio", fechaInicio)
-	            .setParameter("fechaFin", fechaFin)
-	            .uniqueResult();
-	    
-	    session.getTransaction().commit();
-	    ch.cerrarConexion();
-	    
-	    return totalTurnos != null ? totalTurnos : 0;
+		ConfigHibernate ch = new ConfigHibernate();
+		Session session = ch.abrirConexion();
+
+		session.beginTransaction();
+
+		// Consulta HQL para obtener la cantidad de turnos en el estado especificado
+		// entre las fechas especificadas
+		Long cantidadTurnos = (Long) session.createQuery(
+				"SELECT COUNT(t) FROM Turno t " +
+						"WHERE t.estado = :estado " +
+						"AND t.fecha BETWEEN :fechaInicio AND :fechaFin")
+				.setParameter("estado", estado)
+				.setParameter("fechaInicio", fechaInicio)
+				.setParameter("fechaFin", fechaFin)
+				.uniqueResult();
+
+		session.getTransaction().commit();
+		ch.cerrarConexion();
+
+		// Obtener el total de turnos entre las fechas dadas
+		long totalTurnos = obtenerTotalTurnos(fechaInicio, fechaFin);
+
+		// Calcular el porcentaje de turnos en el estado especificado
+		return (double) cantidadTurnos / totalTurnos * 100;
 	}
 
+	private long obtenerTotalTurnos(String fechaInicio, String fechaFin) {
+		ConfigHibernate ch = new ConfigHibernate();
+		Session session = ch.abrirConexion();
+
+		session.beginTransaction();
+
+		// Consulta HQL para obtener el total de turnos entre las fechas especificadas
+		Long totalTurnos = (Long) session.createQuery(
+				"SELECT COUNT(t) FROM Turno t " +
+						"WHERE t.fecha BETWEEN :fechaInicio AND :fechaFin")
+				.setParameter("fechaInicio", fechaInicio)
+				.setParameter("fechaFin", fechaFin)
+				.uniqueResult();
+
+		session.getTransaction().commit();
+		ch.cerrarConexion();
+
+		return totalTurnos != null ? totalTurnos : 0;
+	}
+
+	public double getPorcentajeTotalTurnosPorEstado(EstadoTurno estado, String fechaIni, String fechaFin) {
+		double porcentajeT = 0;
+		int cantidadTurnos = 0;
+		int totalTurnos = 1;
+
+		ConfigHibernate ch = new ConfigHibernate();
+		Session session = ch.abrirConexion();
+		session.beginTransaction();
+		
+		@SuppressWarnings("unchecked")
+		List<Object[]> objTurnos = (List<Object[]>) session.createQuery(
+				"SELECT t FROM Turno t WHERE t.fecha BETWEEN :fechaInicio AND :fechaFin")
+				.setParameter("fechaInicio", fechaIni)
+				.setParameter("fechaFin", fechaFin)
+				.list();
+
+		session.getTransaction().commit();
+		ch.cerrarConexion();
+
+		for (Object objTurno2 : objTurnos) {
+			Turno turno = (Turno) objTurno2;
+			if (turno.getEstado().equals(estado))
+				cantidadTurnos++;
+		}
+		if (cantidadTurnos > 0) {
+			totalTurnos = objTurnos.size();
+			porcentajeT = (double) cantidadTurnos / totalTurnos * 100;
+		}
+
+		return porcentajeT;
+	}
 }
